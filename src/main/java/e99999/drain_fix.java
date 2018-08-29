ae7ffb8aa9f6ac5f6d9fae269e3fd807b0207bc9
e99999 <26906600+e99999@users.noreply.github.com>
2018-08-29 12:35:00 -0700

    updated drain crafting recipe
    using vanilla iron bars instead of iron sticks/rods.

--- src/main/java/e99999/drain_fix.java
+++ src/main/java/e99999/drain_fix.java
@@ -4,12 +4,14 @@ import gregtech.api.enums.ItemList;
 import gregtech.api.enums.Materials;
 import gregtech.api.enums.OrePrefixes;
 import gregtech.api.util.GT_ModHandler;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
 
 /** Simple fix to make drain an early game item */
 
 public class drain_fix
-    implements Runnable {
-        public void run() {
-            GT_ModHandler.addCraftingRecipe(ItemList.Cover_Drain.get(1L, new Object[0]), GT_ModHandler.RecipeBits.NOT_REMOVABLE | GT_ModHandler.RecipeBits.REVERSIBLE | GT_ModHandler.RecipeBits.BUFFERED, new Object[]{"BBB", "BPB", "BBB", Character.valueOf('P'), OrePrefixes.pipeMedium.get(Materials.Bronze), Character.valueOf('B'), OrePrefixes.stick.get(Materials.AnyIron)});
+        implements Runnable {
+            public void run() {
+                GT_ModHandler.addCraftingRecipe(ItemList.Cover_Drain.get(1L, new Object[0]), GT_ModHandler.RecipeBits.NOT_REMOVABLE | GT_ModHandler.RecipeBits.REVERSIBLE | GT_ModHandler.RecipeBits.BUFFERED, new Object[]{"BBB", "BPB", "BBB", Character.valueOf('P'), OrePrefixes.pipeMedium.get(Materials.Bronze), Character.valueOf('B'), new ItemStack(Blocks.iron_bars, 1, 0) });
     }
 }

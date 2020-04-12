fc8f15cb352d3013fd42174f7ab24e01a884ddc4
DbpGaming <LbpDbpGaming@gmail.com>
2020-04-12 08:50:05 -0500

    add iridium fluid multipipes

--- src/main/java/gregtech/loaders/preload/GT_Loader_MetaTileEntities.java
+++ src/main/java/gregtech/loaders/preload/GT_Loader_MetaTileEntities.java
@@ -1474,6 +1474,7 @@ public class GT_Loader_MetaTileEntities implements Runnable {
         generateFluidMultiPipes(Materials.IronWood, Materials.IronWood.mName, "IronWood", 5280, 120, 1975, true);
         generateFluidMultiPipes(Materials.Invar, Materials.Invar.mName, "Invar", 5285, 60, 5335, true);
         generateFluidMultiPipes(Materials.TinAlloy, Materials.TinAlloy.mName, "Tin Alloy", 5290, 37, 1572, true);
+        generateFluidMultiPipes(Materials.Iridium, Materials.Iridium.mName, "Iridium", 5295, 150, 3398, true);
 
         GT_Values.RA.addAssemblerRecipe(GT_OreDictUnificator.get(OrePrefixes.pipeSmall, Materials.TungstenSteel, 1L), ItemList.Electric_Pump_EV.get(1L, new Object[0]), GT_OreDictUnificator.get(OrePrefixes.pipeSmall, Materials.Ultimate, 1L), 300, 96);
         GT_Values.RA.addAssemblerRecipe(GT_OreDictUnificator.get(OrePrefixes.pipeMedium, Materials.TungstenSteel, 1L), ItemList.Electric_Pump_IV.get(1L, new Object[0]), GT_OreDictUnificator.get(OrePrefixes.pipeMedium, Materials.Ultimate, 1L), 400, 148);

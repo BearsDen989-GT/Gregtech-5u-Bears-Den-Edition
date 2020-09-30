57bdf9f2a94ff6eef6555cea4ba3202eab0c7999
Ethryan <dborrfors@gmail.com>
2020-09-30 09:14:12 +0200

    Subscript for chemical formulas part 1

--- src/main/java/gregtech/api/util/GT_Utility.java
+++ src/main/java/gregtech/api/util/GT_Utility.java
@@ -2351,6 +2351,14 @@ public class GT_Utility {
             }
         }
 
+        public static String toSubscript(long no){
+            char[] chars=Long.toString(no).toCharArray();
+            for(int i=0;i<chars.length;i++){
+                chars[i]+=8272;
+            }
+            return new String(chars);
+        }
+
         static final class BullshitIteratorB implements IBullshit {
             public EntityLivingBase mPlayer;
             public Entity mEntity;

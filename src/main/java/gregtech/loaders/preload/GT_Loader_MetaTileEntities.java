922f159d6b66f0d3ebb34c162739919daeae5e8e
Christoph Müller <debuas1999@gmail.com>
2024-01-09 21:48:34 +0100

    added Invar Fluid Pipe

--- src/main/java/gregtech/loaders/preload/GT_Loader_MetaTileEntities.java
+++ src/main/java/gregtech/loaders/preload/GT_Loader_MetaTileEntities.java
@@ -3825,6 +3825,10 @@ public class GT_Loader_MetaTileEntities implements Runnable { // TODO CHECK CIRC
             220000,
             2147483647,
             true);
+        //BDE Changes Fluid Pipes
+        generateFluidPipes(Materials.Invar,Materials.Invar.mName,5320,240,2395,true);
+        generateFluidMultiPipes(Materials.Invar,Materials.Invar.mName,5325,240,2395,true);
+
 
         generateItemPipes(Materials.Brass, Materials.Brass.mName, 5602, 1);
         generateItemPipes(Materials.Electrum, Materials.Electrum.mName, 5612, 2);
